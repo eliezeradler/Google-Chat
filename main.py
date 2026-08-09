e753767807769e5fdae9bdb4ec9442c0500744d3
eliezeradler <aa0527649130@gmail.com>
2026-08-09 18:29:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -322,7 +322,8 @@ if __name__ == '__main__':
         ('spaces/AAQA6TXPw-g', 'spaces/AAQACMVYKrk'),
         ('spaces/AAQAW4OH4YE', 'spaces/AAQAOS_WMkw'),
         ('spaces/AAQACUY6t3I', 'spaces/AAQAWW6csTw'),
-        ('spaces/AAQArWIpnWI', 'spaces/AAQAq5S0W9Q')
+        ('spaces/AAQArWIpnWI', 'spaces/AAQAq5S0W9Q'),
+        ('spaces/AAQApiJLCjU', 'spaces/AAQArtcCJH0'),
     ]
     
     chat_service, creds = authenticate_google_chat()
